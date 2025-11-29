4327f02d561fde58a3310553cb0888c722a57ba1
Sam15246 <21f1004482@ds.study.iitm.ac.in>
2025-11-29 17:39:59 +0530

    fixing import path for deployment

--- deployment/app.py
+++ deployment/app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# This allows us to import modules from the 'src' folder
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import gradio as gr
 from inference import predict
 
