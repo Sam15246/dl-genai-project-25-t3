cc364e4490f94520bd49bc71f079d1497a6776dc
Sam15246 <21f1004482@ds.study.iitm.ac.in>
2025-11-29 17:58:16 +0530

    fixing import path for deployment 4.0

--- src/inference.py
+++ src/inference.py
@@ -1,4 +1,5 @@
 import torch
+import os
 from transformers import AutoTokenizer
 from .models.transformer_model import load_transformer
 from .config import TRANSFORMER_NAME, LABELS, DEVICE
@@ -7,8 +8,10 @@ from .config import TRANSFORMER_NAME, LABELS, DEVICE
 tokenizer = AutoTokenizer.from_pretrained(TRANSFORMER_NAME)
 model = load_transformer(TRANSFORMER_NAME, len(LABELS))
 
+model_path = os.path.join(os.path.dirname(__file__), "..", "saved_models", "roberta_single_state.pth")
+
 # Load trained weights
-model.load_state_dict(torch.load("../saved_models/roberta_single_state.pth", map_location=DEVICE))
+model.load_state_dict(torch.load(model_path, map_location=DEVICE))
 model.to(DEVICE)
 model.eval()
 
